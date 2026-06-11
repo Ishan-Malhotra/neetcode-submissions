18cf457d59d2a88767951d433d64810a4edc46ae
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-06-11 19:32:59 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) 
+    {
+        set <int> s;
+
+        for(int num : nums)
+        {
+            s.insert(num);
+        }
+        //all the elements are now sorted in ascending order in our sorted set
+        //now we need to find the min element, which is the first element
+
+        return *s.begin();
+    
+    }
+};

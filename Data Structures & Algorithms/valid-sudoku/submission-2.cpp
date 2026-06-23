6fad74eefd33ad918f081c0b19c6b193a166eccf
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-06-23 20:16:48 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        //check for rows
+        //check for columns
+        //check for grids
+
+        //1. row
+        for(int row = 0; row < 9; row++)
+        {
+            unordered_set <int> seen;
+
+            for(int i = 0; i < 9; i++)
+            {
+                if(board[row][i] == '.')
+                continue;
+                
+                if(seen.count(board[row][i]))
+                return false;
+                else
+                seen.insert(board[row][i]);
+            }
+        }
+
+        //2. column
+        for(int column = 0; column < 9; column++)
+        {
+            unordered_set <int> seen;
+
+            for(int i = 0; i < 9; i++)
+            {
+                if(board[i][column] == '.')
+                continue;
+                
+                if(seen.count(board[i][column]))
+                return false;
+                else
+                seen.insert(board[i][column]);
+            }
+        }
+
+        //subboxes
+
+        for(int box = 0; box < 9; box++)
+        {
+            unordered_set <int> seen;
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    int row = (box/3) * 3 + i;
+                    int column = (box % 3) * 3 + j;
+
+
+                    if (board[row][column] == '.')
+                    continue;
+
+                    if(seen.count(board[row][column]))
+                    return false;
+                    else
+                    seen.insert(board[row][column]);
+                }
+            }
+            
+        }
+     
+        
+        
+        return true;
+    }
+};

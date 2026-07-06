1396c7716717d6c260aff9e70cf83dae8c7cf38b
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-07-06 19:33:57 +0530

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+        sort(candidates.begin(), candidates.end());
+        vector<int> cur;
+        dfs(0, cur, candidates, target, 0);
+        return res;
+
+    }
+
+    void dfs(int i, vector<int> &cur, vector <int> &candidates, int target, int total)
+    {
+        if(total == target)
+        {
+            res.push_back(cur);
+            return;
+        }
+
+        if(i >= candidates.size() || total > target)
+        {
+            return;
+        }
+
+        cur.push_back(candidates[i]);
+        dfs(i+1, cur, candidates, target, total + candidates[i]);
+
+        cur.pop_back();
+
+        while(i+1 < candidates.size() && candidates[i] == candidates[i+1])
+        {
+            i++;
+        }
+
+        dfs(i+1, cur, candidates, target, total);
+
+    }
+};

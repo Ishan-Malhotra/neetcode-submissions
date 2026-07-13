6dd9720c443c3fc666d39250a26f373b94f173e6
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-07-13 19:29:17 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numDecodings(string s) 
+    {
+        unordered_map <int, int> dp;
+        dp[s.size()] = 1;
+        return dfs(s, 0, dp);
+    }
+
+    int dfs(string &s, int i, unordered_map<int, int> &dp)
+    {
+        if(dp.count(i))
+        {
+            return dp[i];
+        }
+
+        if(s[i] == '0')
+        {
+            return 0;
+        }
+
+        //taking 1 number at once
+        int res = dfs(s, i+1, dp);
+
+        //taking two numbers at once
+        if (i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] < '7'))
+        {
+            res = res + dfs(s, i+2, dp);
+        }
+
+        dp[i] = res;
+        return res;
+    }
+};

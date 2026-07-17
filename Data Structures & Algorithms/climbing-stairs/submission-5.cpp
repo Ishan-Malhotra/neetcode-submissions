c14026d02371ee7419615a83ae87c4fdebf15044
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-07-17 23:02:02 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    vector <int> cache;
+
+    int climbStairs(int n) 
+    {
+        cache.resize(n, -1);
+        return dfs(0,n);
+    }
+
+    int dfs(int i, int n)
+    {
+        if(i >= n)
+        return i == n;
+
+        if(cache[i] != -1)
+        return cache[i];
+
+        cache[i]= dfs(i+1, n) + dfs(i+2, n);
+        return cache[i];
+    }
+};

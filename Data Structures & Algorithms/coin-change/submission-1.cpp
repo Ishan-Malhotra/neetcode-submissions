d2c73120b3a511bc6f0f50ab9bd60f36033b45ae
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-07-23 02:07:47 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    unordered_map <int, int> memo;
+
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int minCoins = dfs(coins, amount);
+        return minCoins >= 1e9 ? -1 : minCoins;
+        
+    }
+
+
+//return the fewest number of coins you need to make up the target
+
+    int dfs(vector<int> &coins, int amount)
+    {
+        if(amount == 0)
+        return 0;
+
+        if(memo.find(amount) != memo.end())
+        return memo[amount];
+
+        int res = INT_MAX;
+
+        for(int coin : coins)
+        {
+            if(amount - coin >= 0)
+            {
+                int result = dfs(coins, amount-coin);
+
+                if(result != INT_MAX)
+                {
+                    res = min(res, 1 + result);
+                }
+            }
+        }
+        memo[amount] = res;
+        return res;
+
+    }
+};

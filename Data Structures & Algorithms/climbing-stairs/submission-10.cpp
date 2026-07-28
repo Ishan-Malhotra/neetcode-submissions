e08c26f48cd141ed645b9dec9089ee98d089f895
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-07-28 15:35:20 +0530

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> memo;
+    int climbStairs(int n) 
+    {
+        memo.resize(n, -1);
+        return dfs(n , 0);
+    }
+
+    int dfs(int n, int i)
+    {
+        if(i >= n)
+        return i == n;
+
+        if(memo[i] != -1)
+        return memo[i];
+
+        memo[i] = dfs(n, i+1) + dfs(n, i+2);
+        return memo[i];
+        
+    }
+};

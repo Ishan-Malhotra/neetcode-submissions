28c40f250bf0d473828716a427058df777a85416
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-08-03 05:46:55 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) 
+    {
+        unordered_set <int> list;
+
+        for(int n:nums)
+        {
+            if(list.count(n))
+                return true;
+            
+            else
+            list.insert(n);
+        }
+        return false;
+        
+    }
+};

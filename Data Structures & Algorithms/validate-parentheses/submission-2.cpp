deb45783a9b3aaa5821b5bcd914ad95ec194094b
Ishan-Malhotra <ishanmalhotra2004@gmail.com>
2026-08-13 22:42:37 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack <char> myStack;
+
+        for(const auto & ch: s)
+        {
+            if( ch == '(' || ch == '{' || ch == '[')
+            {
+                myStack.push(ch);
+            }
+
+            else if (ch == ')')
+            {
+                if(myStack.empty() || myStack.top() != '(')
+                return false;
+
+                myStack.pop();
+            }
+
+            else if (ch == ']')
+            {
+                if(myStack.empty() || myStack.top() != '[')
+                return false;
+
+                myStack.pop();
+            }
+
+             else if (ch == '}')
+            {
+                if(myStack.empty() || myStack.top() != '{')
+                return false;
+
+                myStack.pop();
+            }
+        }
+
+        if(myStack.empty())
+        return true;
+        else
+        return false;
+        
+    }
+};
